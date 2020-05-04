3730d80e448230c6cdf7a9fb611287b5a426afcb
Nick Klein <1294247+nickklein@users.noreply.github.com>
2020-05-03 19:34:42 -0700

    improve responsiveness, make tables responsive, add mobile nav bar

--- resources/js/components/devices/DeviceJobs.vue
+++ resources/js/components/devices/DeviceJobs.vue
@@ -1,24 +1,26 @@
 <template>
-    <table class="table">
-        <thead class="thead">
-            <th>ID</th>
-            <th>Device Name</th>
-            <th>Key</th>
-            <th>Value</th>
-            <th>Status</th>
-            <th>Date Created</th>
-        </thead>
-        <tbody>
-            <tr v-for="item in items" v-bind:key="item.device_job_id">
-                <td>{{item.device_job_id}}</td>
-                <td>{{item.device_name}}</td>
-                <td>{{item.key}}</td>
-                <td>{{item.value}}</td>
-                <td>{{item.status}}</td>
-                <td>{{item.created_at}}</td>
-            </tr>
-        </tbody>
-    </table>
+    <div class="table-responsive-md">
+        <table class="table">
+            <thead class="thead">
+                <th>ID</th>
+                <th>Device Name</th>
+                <th>Key</th>
+                <th>Value</th>
+                <th>Status</th>
+                <th>Date Created</th>
+            </thead>
+            <tbody>
+                <tr v-for="item in items" v-bind:key="item.device_job_id">
+                    <td>{{item.device_job_id}}</td>
+                    <td>{{item.device_name}}</td>
+                    <td>{{item.key}}</td>
+                    <td>{{item.value}}</td>
+                    <td>{{item.status}}</td>
+                    <td>{{item.created_at}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 <script>
 export default {
